91826088a9dfa52e036bfe95bd4be1d6a76718f4
David Vilchez <dvilchez@xuaps.com>
2014-06-11 12:35:06 +0200

    initial try

---
+++ picto_downloader/picto_downloader/Sclera.fs
@@ -0,0 +1,22 @@
+﻿module Picto.Sclera
+
+open HtmlAgilityPack.FSharp
+open FSharp.Data
+
+let getCategoriesListPage  =
+    "http://www.sclera.be/en/picto/cat_overview" 
+    |> Http.AsyncRequestString
+
+let getCategories categoriesPage=
+    categoriesPage
+    |> createDoc
+    |> descendants "ul"
+    |> Seq.filter (hasClass "clearfix")
+    |> Seq.head
+    |> followingSibling "li"
+    |> descendants "a"
+    |> Seq.head
+    |> innerText
+
+
+
---
+++ picto_downloader/picto_downloader/ScleraTest.fs
@@ -0,0 +1,12 @@
+﻿namespace picto_downloader
+open System
+open NUnit.Framework
+open Picto.Sclera
+
+[<TestFixture>]
+type ScleraTest() = 
+
+    [<Test>]
+    member x.GetCategories() =
+        Assert.AreSame(["Abstract";"Animals";], getCategories)
+

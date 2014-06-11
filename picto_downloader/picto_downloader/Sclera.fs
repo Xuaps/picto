27a4d3a6930fe2a7d14f11085dbe0fb3b6dd9cdd
David Vilchez <dvilchez@xuaps.com>
2014-06-11 16:20:37 +0200

    get categories from sclera

--- picto_downloader/picto_downloader/Sclera.fs
+++ picto_downloader/picto_downloader/Sclera.fs
@@ -3,20 +3,23 @@
 open HtmlAgilityPack.FSharp
 open FSharp.Data
 
-let getCategoriesListPage  =
-    "http://www.sclera.be/en/picto/cat_overview" 
-    |> Http.AsyncRequestString
+type Categories() =
+    static member getCategoriesListPage  =
+        "http://www.sclera.be/en/picto/cat_overview" 
+        |> Http.AsyncRequestString
 
-let getCategories categoriesPage=
-    categoriesPage
-    |> createDoc
-    |> descendants "ul"
-    |> Seq.filter (hasClass "clearfix")
-    |> Seq.head
-    |> followingSibling "li"
-    |> descendants "a"
-    |> Seq.head
-    |> innerText
+    static member getCategories categoriesPage =
+        categoriesPage
+        |> createDoc
+        |> descendants "ul"
+        |> Seq.filter (hasClass "clearfix")
+        |> Seq.head
+        |> descendants "a"
+        |> Seq.map(fun a->[|innerText a;attr "href" a|])
+        |> Seq.toArray
 
 
-
+    static member categories =
+        Categories.getCategoriesListPage
+        |> Async.RunSynchronously
+        |> Categories.getCategories
--- picto_downloader/picto_downloader/ScleraTest.fs
+++ picto_downloader/picto_downloader/ScleraTest.fs
@@ -2,11 +2,13 @@
 open System
 open NUnit.Framework
 open Picto.Sclera
+open FSharp.Data
+open HtmlAgilityPack.FSharp
 
 [<TestFixture>]
 type ScleraTest() = 
 
     [<Test>]
     member x.GetCategories() =
-        Assert.AreSame(["Abstract";"Animals";], getCategories)
-
+        Assert.AreEqual([|[|"Abstract";"/cat/1"|];[|"Animals";"/cat/2"|]|], Categories.getCategories "<html><head></head><body><ul class='clearfix'><li><a href='/cat/1'>Abstract</a></li><li><a href='/cat/2'>Animals</a></li></ul></body></html>")
+    
